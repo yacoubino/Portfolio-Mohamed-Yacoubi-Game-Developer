26de62bc9611616aa2c6479e6464bce830c6a451
yacoubino <yacoubino23@gmail.com>
2026-02-13 19:53:05 +0100

    JJ4

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+// LIGHTBOX
 function openLightbox(img) {
-    const lightbox = document.getElementById("lightbox");
-    const lightboxImg = document.getElementById("lightbox-img");
-
-    lightboxImg.src = img.src;
-    lightbox.style.display = "flex";
+    document.getElementById("lightbox").style.display = "flex";
+    document.getElementById("lightbox-img").src = img.src;
 }
 
-function closeLightbox(e) {
-    if (
-        e.target.classList.contains("lightbox") ||
-        e.target.classList.contains("lightbox-close")
-    ) {
+function closeLightbox(event) {
+    if (event.target.id === "lightbox" || event.target.className === "lightbox-close") {
         document.getElementById("lightbox").style.display = "none";
     }
 }
+
+// FADE IN ON SCROLL
+const faders = document.querySelectorAll(".fade-in");
+
+const appearOptions = {
+    threshold: 0.3
+};
+
+const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+    });
+}, appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+});
